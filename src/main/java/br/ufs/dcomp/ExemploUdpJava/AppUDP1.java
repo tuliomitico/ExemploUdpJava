8a46dcd966ea16e1822c6f7a34389f1eee6cc1d8
Ubuntu <ubuntu@ip-172-31-84-79.ec2.internal>
2019-11-02 00:56:04 +0000

    chat atrapalhado

--- src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP1.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP1.java
@@ -1,7 +1,8 @@
 package br.ufs.dcomp.ExemploUdpJava;
 
 import java.net.*;
-
+import static java.lang.System.*;
+import java.util.Scanner;
 public class AppUDP1 {
 
     public static void main(String[] args) throws SocketException {
@@ -9,8 +10,9 @@ public class AppUDP1 {
             System.out.print("[ Alocando porta UDP      ..................  ");
     	    DatagramSocket socket = new DatagramSocket(10000);
             System.out.println("[OK] ]");
-            
-            String msg = "Olá!!!";
+            while (true){
+            Scanner sc = new Scanner(System.in);
+            String msg = sc.nextLine();
             
             byte[] msg_buf = msg.getBytes();
             int msg_size = msg_buf.length;
@@ -24,7 +26,19 @@ public class AppUDP1 {
             System.out.print("[ Enviando datagrama UDP  ..................  ");
             socket.send(pack);
             System.out.println("[OK] ]");
-
+            byte[] buffer = new byte[128]
+            DatagramPacket pacote = new DatagramPacket(buffer,buffer.length)
+            socket.receive(pacote);
+            
+            InetAddress origem_end = pack.getAddress();
+            int porta_origem = pack.getPort();
+            byte[] dado_recebido = pack.getData();
+            String msg_recebida = new String(dado_recebido);
+            
+            System.out.println("Mensagem:"+"\t" +msg_recebida);
+            System.out.println("Endereço de origem: "+"\t" +origem_end);
+            System.out.println("Porta de Origem: " + "\t" +porta_origem);
+            }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }    
--- src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP2.java
+++ src/main/java/br/ufs/dcomp/ExemploUdpJava/AppUDP2.java
@@ -1,6 +1,7 @@
 package br.ufs.dcomp.ExemploUdpJava; 
 
 import java.net.*;
+import java.util.Scanner;
 
 public class AppUDP2 {
 
@@ -10,8 +11,8 @@ public class AppUDP2 {
             System.out.print("[ Alocando porta UDP                  ..................  ");
     	    DatagramSocket socket = new DatagramSocket(20000);
             System.out.println("[OK] ]");
-
-            byte[] buf = new byte[20];
+            while (true){
+            byte[] buf = new byte[128];
             DatagramPacket pack = new DatagramPacket(buf, buf.length);
 
             System.out.print("[ Aguardando recebimento de mensagem  ..................  ");
@@ -27,6 +28,18 @@ public class AppUDP2 {
             System.out.println("  Endereço de origem:   "+origin_address.getHostAddress());
             System.out.println("  Porta de origem:      "+origin_port);
             
+            Scanner input = new Scanner(System.in);
+            String retweet = input.nextLine();
+            
+            byte[] retweet_buf = retweet.getBytes();
+            int retweet_size = retweet_buf.length;
+            
+            InetAddress destino_end = origin_address;
+            
+            DatagramPacket pacote = new DatagramPacket(retweet_buf,retweet_size,origin_address,origin_port);
+            System.out.println("Vai!");
+            socket.send(pacote);
+            }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }    
